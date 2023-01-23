bdd17c8ec9aefdf195101b8acec01ac5e49f5f84
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-01-23 13:58:59 -0600

    finished the database exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     album  VARCHAR(50) NOT NULL,
-    release_date  DATE NOT NULL,
+    release_date  INT UNSIGNED NOT NULL,
     sales DOUBLE NOT NULL,
     genre TEXT NOT NULL,
     PRIMARY KEY (id)
