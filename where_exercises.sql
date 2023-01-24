342a6aaf068da9afbadbc973c16ea8c6e5de3afc
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-01-24 14:47:15 -0600

    finished the where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+-- Part I
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+-- Part II
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name like '%q%' AND last_name NOT LIKE '%qu%';
